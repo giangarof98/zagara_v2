2ec14a8f642b355928e16039fd9115c2af4021f4
Gianmarco G <giangarof98@gmail.com>
2023-07-20 21:12:41 -0400

    middleware

--- back/server.js
+++ back/server.js
@@ -1,26 +1,23 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
+import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 connectDB();
 
 import express from 'express';
-import products from './data/products.js';
 
+import productRoutes from './routes/productRoutes.js'
 
 const port = process.env.PORT || 80000;
 const app = express();
 
 app.get('/', (req,res) => {
     res.send('api running...')
-})
+});
 
-app.get('/api/products', (req,res) => {
-    res.json(products)
-})
+app.use('/api/products', productRoutes)
 
-app.get('/api/products/:id', (req,res) => {
-    const product = products.find((p) => p._id === req.params.id)
-    res.json(product)
-})
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`running on port ${port}`))
