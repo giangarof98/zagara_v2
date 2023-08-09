61e431ee65cb9efef36029d07113780bdce6f17b
Gianmarco G <giangarof98@gmail.com>
2023-08-08 23:57:50 -0400

    order screen style

--- front/src/screen/OrderScreen.jsx
+++ front/src/screen/OrderScreen.jsx
@@ -8,11 +8,42 @@ import {useGetOrderDetailsQuery} from '../slices/orderApiSlice'
 
 export default function OrderScreen(){
     const {id:orderId} = useParams();
-    const {data:order, refetch, isLoading, isError} = useGetOrderDetailsQuery(orderId)
+    const {data: order, refetch, isLoading, error} = useGetOrderDetailsQuery(orderId)
     console.log(order)
-    return (
+
+    return error ? <Loader/> : error ? <Message variant="danger" /> : (
         <>
-            <h1>Order Screen</h1>
+            <h1>Order {order._id}</h1>
+            <Row>
+                <Col md={8}>
+                    <ListGroup.Item>
+                        <h2>Shipping</h2>
+                        <p><strong>Name:</strong> {order.user.name}</p>
+                        <p><strong>Email:</strong> {order.user.email}</p>
+                        <p><strong>Address:</strong> 
+                            {order.shippingAddress.address}, {order.shippingAddress.city}{' '}
+                            {order.shippingAddress.postalCode},{' '}
+                            {order.shippingAddress.country}
+                        </p>
+                        {order.isDelivered ? (
+                            <Message variant='success'>
+                                Delivered on {order.deliveredAt}
+                            </Message>
+                        ) : (
+                            <Message variant='danger'>
+                                Not Delivered
+                            </Message>
+                        )}
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        
+                    </ListGroup.Item>
+                </Col>
+
+
+
+                <Col md={4}>Column</Col>
+            </Row>
         </>
     )
 }
